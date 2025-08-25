e72c826759d22996706b1ff3a9c820212b658e30
Santosh Kulkarni <santoshvqa@gmail.com>
2025-08-25 13:26:13 +0530

    Update TestRunner.java

--- src/test/java/com/kushi/runners/TestRunner.java
+++ src/test/java/com/kushi/runners/TestRunner.java
@@ -7,10 +7,17 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", 
-		"com.kushi.utility.CustomListener" }, features = { "src/test/java/features" }, glue = { "com.kushi.stepdefinitions" }
-		, monochrome = true, snippets = SnippetType.CAMELCASE, tags = "@BookerAPI", publish = true)
+@CucumberOptions(
+    features = { "src/test/java/features" },
+    glue = { "com.kushi.stepdefinitions" },
+    plugin = {
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        "com.kushi.utility.CustomListener"
+    },
+    monochrome = true,
+    snippets = SnippetType.CAMELCASE,
+    tags = "@BookerAPI",
+    publish = false // changed from true to false
+)
 public class TestRunner {
-
 }
