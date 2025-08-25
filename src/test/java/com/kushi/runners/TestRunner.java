e5dc6de3c85687eeaff474566b9aa5eb61ee1abc
Santosh Kulkarni <santoshvqa@gmail.com>
2025-08-25 13:23:35 +0530

    Update TestRunner.java

--- src/test/java/com/kushi/runners/TestRunner.java
+++ src/test/java/com/kushi/runners/TestRunner.java
@@ -7,12 +7,9 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty:target/cucumber/cucumber.txt",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "html:target/cucumber/report",
-		"json:target/cucumber/cucumber.json",
+@CucumberOptions(plugin = {
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", 
 		"com.kushi.utility.CustomListener" }, features = { "src/test/java/features" }, glue = { "com.kushi.stepdefinitions" }
-// ,dryRun = true, It is used to check if any stepdefinitions missed to
-// implement
 		, monochrome = true, snippets = SnippetType.CAMELCASE, tags = "@BookerAPI", publish = true)
 public class TestRunner {
 
